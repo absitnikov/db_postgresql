2fdf3efc2db406ca6cb3cc8f942de4eb12f36810
absitnikov <ab.sitnikov@mail.ru>
2022-09-01 12:07:55 +0500

    insert, select

--- select.sql
+++ select.sql
@@ -5,7 +5,7 @@ SELECT  track_name , track_duration FROM track
 WHERE track_duration = (SELECT MAX(track_duration) FROM track);
 
 SELECT track_name FROM track
-WHERE track_duration >= 3.50
+WHERE track_duration >= 3.30
 
 SELECT collection_name FROM collection
 WHERE EXTRACT (YEAR FROM collection_year) BETWEEN 2018 AND 2020;
@@ -15,4 +15,3 @@ WHERE musician_name NOT LIKE '% %';
 
 SELECT track_name FROM track
 WHERE track_name  LIKE '%my%' OR track_name LIKE '%моё%';
-
