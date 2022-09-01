d4bebf33098539f049a9282a106f2a6b1243889c
absitnikov <ab.sitnikov@mail.ru>
2022-09-01 11:40:49 +0500

    insert, select

--- select.sql
+++ select.sql
@@ -1,11 +1,11 @@
 SELECT album_name, album_year FROM album
 WHERE EXTRACT (YEAR FROM album_year) = 2018;
 
-SELECT  track_name , track_duration from track
-where track_duration = (select MAX(track_duration) from track);
+SELECT  track_name , track_duration FROM track
+WHERE track_duration = (SELECT MAX(track_duration) FROM track);
 
-SELECT track_name from track
-where track_duration >= 3.50
+SELECT track_name FROM track
+WHERE track_duration >= 3.50
 
 SELECT collection_name FROM collection
 WHERE EXTRACT (YEAR FROM collection_year) BETWEEN 2018 AND 2020;
